b606c51309f150f09d34e3ed6acacdc579f0051c
TheodorJ <teddyjohansson@live.com>
2018-01-30 15:02:16 -0500

    Added some ECDSA helpers

--- cryptohelpers.py
+++ cryptohelpers.py
@@ -1,7 +1,18 @@
 from Crypto.Hash import SHA256
+from ecdsa import SigningKey, NIST256p
 
 # Take in bytes, output SHA256(bytes)
 def hash(bytes):
     h = SHA256.new()
     h.update(bytes)
     return h.digest()
+
+def gen_privkey():
+    sk = SigningKey.generate(curve=NIST256p)
+    return sk
+
+def gen_pubkey(privkey):
+    return privkey.get_verifying_key()
+
+def sign(privkey, message):
+    return privkey.sign(message)
